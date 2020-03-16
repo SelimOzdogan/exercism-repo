d00cb3a481a2e9b13088efe0f08c0acba504cd9b
Selim <SelimOzdogan@hotmail.com>
2020-03-15 23:59:13 -0700

    run-length-encoding

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -3,10 +3,46 @@
 // convenience to get you started writing code faster.
 //
 
-export const encode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const encode = (decoded = '') => {
+  let encoded = '';
+  let number;
+  let char;
+  if (decoded == encoded)
+    return encoded;
+  for (let value of decoded.split('')) {
+    if (char == undefined) {
+      number = 1;
+      char = value;
+    }
+    else if (value != char) {
+      encoded += (number == 1 ? '' : number) + char;
+      number = 1;
+      char = value;
+    }
+    else {
+      number += 1;
+    }
+  }
+  encoded += (number == 1 ? '' : number) + char;
+  return encoded;
 };
 
-export const decode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const decode = (encoded = '') => {
+  let decoded = '';
+  let number = 0;
+  for (let value of encoded.split('')) {
+    if (value.match(/[a-z]/i) != null || value == ' ') {
+      if (number == '' || number == undefined)
+        number = parseInt(1);
+      else {
+        number = parseInt(number);
+      }
+      decoded += value.repeat(number);
+      number = 0;
+    }
+    else {
+      number += value;
+    }
+  }
+  return decoded;
 };
