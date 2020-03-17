5536759e0a78fa5fc2b2d067f71862916d47cd79
Selim <SelimOzdogan@hotmail.com>
2020-03-17 00:48:27 -0700

    bug fixed

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,8 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  COLORS ={"black":0,"brown":1,"red":2,"orange":3,"yellow":4,"green":5,"blue":6,"violet":7,"grey":8,"white":9])};
+export const colorCode = (color) => {
+  COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+  for (let i = 0; i < COLORS.length; i++) {
+    if (COLORS[i] === color)
+      return i;
+  }
+  return null;
 };
 
-export const COLORS = undefined;
+export let COLORS = undefined;
