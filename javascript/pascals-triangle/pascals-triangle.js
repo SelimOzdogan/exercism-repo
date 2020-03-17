a400af232a04c4e5580bce27f9ccd998228017a1
Selim <SelimOzdogan@hotmail.com>
2020-03-17 13:02:26 -0700

    pascals-triangle finished

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const rows = () => {
-  throw new Error("Remove this statement and implement this function");
+export const rows = (num) => {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    if (i === 0) {
+      arr[i] = [1];
+    }
+    else if (i === 1) {
+      arr[i] = [1, 1];
+    }
+    else {
+      let arrin = [];
+      for (let j = 0; j <= i; j++) {
+        arrin[j] = parseInt(j === 0 ? 0 : arr[i - 1][j - 1]) + parseInt(j === i ? 0 : arr[i - 1][j]);
+      }
+      arr[i] = arrin;
+    }
+  }
+  return arr;
 };
